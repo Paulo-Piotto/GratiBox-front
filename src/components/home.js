a783821827beafd895b37d70d086604ba706f2f6
Paulo <paulo_piotto@hotmail.com>
2021-11-20 16:42:24 -0300

    pagina inicial finalizada

---
+++ src/components/home.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import homeImage from '../assets/meditation_woman.png'
+import { Container, Title, MainButton, SubButton } from "../styles/sharedStyles";
+import { useNavigate } from "react-router-dom";
+
+export default function Home(){
+    const navigate = useNavigate();
+
+    return(
+        <Container>
+            <TextContent>
+                <Title>Bem vindo ao <span>GratiBox</span></Title>
+                <Text>Receba em casa um box com chás, produtos organicos, incensos e muito mais...</Text>
+            </TextContent>
+            <ImageContent>
+                <img src={homeImage} alt=''/>
+                <Buttons>
+                    <MainButton onClick={() => navigate('/sign-up')} >Quero começar</MainButton>
+                    <SubButton onClick={() => navigate('/sign-in')} >Já sou grato</SubButton>
+                </Buttons>
+            </ImageContent>
+        </Container>
+    );
+}
+
+const Text = styled.p`
+    font-size: 18px;
+    text-align: center;
+    margin-bottom: 30px;
+    font-weight: 300;
+`
+
+const TextContent = styled.div`
+    height: 30%;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ImageContent = styled.div`
+    width: 100%;
+    height: 70%;
+    background-color: #4D65A8;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+
+    img{
+        width: 100%;
+    }
+`
+
+const Buttons = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    bottom: 40px;
+`
---
+++ src/styles/sharedStyles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Title = styled.h2`
+    text-align: center;
+    font-size: 28px;
+    margin: 50px 0 45px 0;
+
+    span{
+        font-weight: bold;
+    }
+`
+
+const MainButton = styled.button`
+    border: none;
+    outline: none;
+    border-radius: 10px;
+    padding: 11px;
+    width: 50%;
+    height: 45px;
+    background-color: #8C97EA;
+    font-weight: 700;
+    font-size: 18px;
+    color: white;
+    margin-bottom: 15px;
+`
+
+const SubButton = styled.p`
+    font-size: 18px;
+    font-weight: 700;
+`
+
+export {
+    Container,
+    Title,
+    MainButton,
+    SubButton,
+}
